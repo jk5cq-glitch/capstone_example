0f623109a28da53bcd7342661288325638f4d3b6
jk5cq-glitch <jk5cq@umsystem.edu>
2025-12-11 21:32:25 +0000

    html report please

--- src/app.py
+++ src/app.py
@@ -1,2 +1,31 @@
+import math
+
 def add(a, b):
     return a+b
+
+def sub(a,b):
+    return a-b
+
+def mult(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+def log(a,b):
+    return math.log(a,b)
+
+def squ(a):
+    return a ** 2
+
+def sin(a):
+    return math.sin(a)
+
+def cos(a)
+    return math.cos(a)
+
+def root(a)
+    return math.sqrt(a)
+
+def perc(a, b)
+    return (a / b) * 100  
--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,7 @@ from pathlib import Path
 root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
 
-from app import add
+from app import add, sub, mult, div, squ, root, sin, cos, root, 
 
 def test_add():
     assert add(5, 6) == 11
